9714e57701d3cb53648c1e73df8e8f3f8c93e003
rehanhanar7 <rehanhanar7@gmail.com>
2021-10-05 11:41:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-5gwexq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5gwexq)
---
+++ index.js
@@ -0,0 +1,76 @@
+/**
+ * class, car (name or brandname), Toyota
+ *
+ * price :
+ * when price is added
+ *
+ *
+ */
+
+// function Car(brandname) {
+//   this.brandname = brandname;
+//   // this.price = 10000;
+// }
+
+// var toyota = new Car('Toyota');
+// var ford = new Car('Ford');
+
+// console.log(toyota, ford);
+
+var arr = [1, 2, 5, 4, 1, 2, 8];
+
+// arr['removeDuplicate'] = function () {
+//   console.log('removeDuplicate');
+// };
+
+// Array.prototype.removeDuplicate = function(){
+
+// }
+
+// console.log(typeof arr);
+
+// console.log(arr); // array values
+// console.log(arr.removeDuplicate()); // function
+
+// var temp = arr.removeDuplicate();
+//temp = [1, 2, 5, 4, 8]
+
+console.log('a');
+// const mypromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log('b');
+//     resolve('done');
+//   }, 1000);
+// }).then((res) => {
+//   console.log('c');
+// });
+
+// function mypromisemethod() {
+//   mypromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('b');
+//       resolve('done');
+//     }, 1000);
+//   });
+//   return mypromise;
+// }
+
+// async function myasync() {
+//   result = await mypromisemethod();
+//   console.log('c');
+// }
+
+// myasync();
+
+
+// function sum(a, b){
+//   if(b){
+//       return a + b;
+//   }
+//   return function(b){
+//       return a + b;
+//   }
+// }
+
+// console.log(sum(1,2))
+// console.log(sum(1)(2));
